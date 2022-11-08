4f4bd5ae125494c79fe9d563f5ba0feb8edf01c8
Jack Harmer <j.harmer99@gmail.com>
2022-11-08 15:57:11 +0000

    dark mode toggle added

--- React Calculator/calculator/src/calculator.js
+++ React Calculator/calculator/src/calculator.js
@@ -1,18 +1,40 @@
 import { useState } from "react";
 
-function Button({ className, id, value, onClick }) {
+function Button({ className, id, value, onClick, darkSwitch }) {
     return (
-        <div className={className} id={id} onClick={onClick}>
+        <div
+            className={darkSwitch ? className + " dark" : className}
+            id={id}
+            onClick={onClick}
+        >
             {value}
         </div>
     );
 }
 
+function Switch(props) {
+    return (
+        <div className="switch">
+            <input type="checkbox" checked={props.darkSwitch ? true : false} />
+            <span className="slider round" onClick={props.onClick}></span>
+        </div>
+    );
+}
+
 export default function Calculator() {
-    const [answer, setAnswer] = useState(0);
-    const [expression, setExpression] = useState("babaka");
-    const [computed, setComputed] = useState(false);
+    const [answer, setAnswer] = useState("");
+    const [expression, setExpression] = useState("0");
     const [cleared, setCleared] = useState(true);
+    const [darkSwitch, setDarkSwitch] = useState(false);
+
+    function handleSwitch() {
+        setDarkSwitch(!darkSwitch);
+        if (!darkSwitch) {
+            document.body.classList.add("dark");
+        } else {
+            document.body.classList.remove("dark");
+        }
+    }
 
     function handleClick(value) {
         if (cleared) {
@@ -23,24 +45,11 @@ export default function Calculator() {
                 value === "+"
             ) {
                 setExpression(expression + " " + value + " ");
+                setCleared(false);
             } else if (!isNaN(parseFloat(value)) || value == ".") {
                 setExpression(value);
                 setCleared(false);
             }
-        } else if (computed) {
-            if (
-                value === "/" ||
-                value === "*" ||
-                value === "-" ||
-                value === "+"
-            ) {
-                setExpression(expression + " " + value + " ");
-            } else if (!isNaN(parseFloat(value)) || value == ".") {
-                setAnswer(answer + " " + expression);
-                setExpression(value);
-            }
-
-            setComputed(false);
         } else {
             if (
                 value === "/" ||
@@ -70,8 +79,11 @@ export default function Calculator() {
     }
 
     function handleClick_Clear() {
+        if (cleared) {
+            setAnswer("");
+        }
         setExpression(0);
-        setComputed(true);
+        setCleared(true);
     }
 
     function handleClick_DeleteLast() {
@@ -84,10 +96,10 @@ export default function Calculator() {
     }
 
     function handleClick_equals() {
-        let ans = evaluate(expression);
-        setAnswer(expression + " =");
+        let ans = Math.round(evaluate(expression) * 100) / 100;
+        setAnswer(expression + " = " + ans);
         setExpression(ans);
-        setComputed(true);
+        setCleared(true);
     }
 
     function evaluate(toEvaluate) {
@@ -176,18 +188,23 @@ export default function Calculator() {
     return (
         <div className="main ">
             <div className="d-flex justify-content-center">
-                <div className="calculator">
+                <div className={darkSwitch ? "calculator dark" : "calculator"}>
                     <div className="screen p-5 pb-3">
                         <div id="previous">{answer}</div>
                         <input id="current" value={expression} />
                     </div>
-                    <div className="calc-body p-5">
+                    <div
+                        className={
+                            darkSwitch ? "dark calc-body p-5" : "calc-body p-5"
+                        }
+                    >
                         <div className="d-flex justify-content-between mb-4">
                             <Button
                                 className="calc-btn thingy"
                                 id="clear"
                                 value="C"
                                 onClick={handleClick_Clear}
+                                darkSwitch={darkSwitch}
                             />
 
                             <Button
@@ -195,18 +212,21 @@ export default function Calculator() {
                                 id="("
                                 value="("
                                 onClick={() => handleClick("(")}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn thingy"
                                 id=")"
                                 value=")"
                                 onClick={() => handleClick(")")}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn thingy"
                                 id="delete"
                                 value="del"
                                 onClick={handleClick_DeleteLast}
+                                darkSwitch={darkSwitch}
                             />
                         </div>
                         <div className="d-flex justify-content-between mb-4">
@@ -215,24 +235,28 @@ export default function Calculator() {
                                 id="7"
                                 value="7"
                                 onClick={() => handleClick("7")}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn expression"
                                 id="8"
                                 value="8"
                                 onClick={() => handleClick("8")}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn expression"
                                 id="9"
                                 value="9"
                                 onClick={() => handleClick("9")}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn operator expression"
                                 id="divide"
                                 value="/"
                                 onClick={() => handleClick("/")}
+                                darkSwitch={darkSwitch}
                             />
                         </div>
                         <div className="d-flex justify-content-between mb-4">
@@ -241,24 +265,28 @@ export default function Calculator() {
                                 id="4"
                                 value="4"
                                 onClick={() => handleClick("4")}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn expression"
                                 id="5"
                                 value="5"
                                 onClick={() => handleClick("5")}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn expression"
                                 id="6"
                                 value="6"
                                 onClick={() => handleClick("6")}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn operator expression"
                                 id="mult"
                                 value="*"
                                 onClick={() => handleClick("*")}
+                                darkSwitch={darkSwitch}
                             />
                         </div>
                         <div className="d-flex justify-content-between mb-4">
@@ -267,24 +295,28 @@ export default function Calculator() {
                                 id="1"
                                 value="1"
                                 onClick={() => handleClick("1")}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn expression"
                                 id="2"
                                 value="2"
                                 onClick={() => handleClick("2")}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn expression"
                                 id="3"
                                 value="3"
                                 onClick={() => handleClick("3")}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn operator expression"
                                 id="minus"
                                 value="-"
                                 onClick={() => handleClick("-")}
+                                darkSwitch={darkSwitch}
                             />
                         </div>
                         <div className="d-flex justify-content-between">
@@ -293,27 +325,36 @@ export default function Calculator() {
                                 id="0"
                                 value="0"
                                 onClick={() => handleClick("0")}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn expression"
                                 id="dot"
                                 value="."
                                 onClick={() => handleClick(".")}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn"
                                 id="equals"
                                 value="="
                                 onClick={handleClick_equals}
+                                darkSwitch={darkSwitch}
                             />
                             <Button
                                 className="calc-btn operator expression"
                                 id="plus"
                                 value="+"
                                 onClick={() => handleClick("+")}
+                                darkSwitch={darkSwitch}
                             />
                         </div>
                     </div>
+                </div>
+            </div>
+            <div className="d-flex justify-content-center">
+                <div className="switch-div">
+                    <Switch darkSwitch={darkSwitch} onClick={handleSwitch} />
                 </div>
             </div>
         </div>
